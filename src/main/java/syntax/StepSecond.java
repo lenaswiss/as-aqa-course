6ebdcdce13bd5a734a819b75cfdc77cf22625423
lena <cherchenko.olena@airslate.com>
2023-03-23 21:48:47 +0100

    Tasks from Step third

--- src/main/java/syntax/StepSecond.java
+++ src/main/java/syntax/StepSecond.java
@@ -1,8 +1,8 @@
 package syntax;
 
 public class StepSecond {
-
     public static void main(String[] args) {
+
         StepSecond stepSecond = new StepSecond();
         stepSecond.triangleArea(3, 4, 3);
     }
@@ -13,10 +13,14 @@ public class StepSecond {
      * For this task use Math library, for square root operation(Math.sqrt(argument) <- it will be magic for now.
      * A little bit later I’ll explain how it works).
      */
-    public void triangleArea(double a, double b, double c){
-       double area = (a+b+c)/2.0d;
-       double result = Math.sqrt(area* (area - a) * (area - b) * (area - c));
+    public void triangleArea(double a, double b, double c) {
+        double area = (a + b + c) / 2.0d;
+        double result = Math.sqrt(area * (area - a) * (area - b) * (area - c));
+        System.out.println("***************Task 1***************");
+        System.out.println("an area of a triangle ");
         System.out.println("the triangle area " + result);
+        System.out.println("************************************");
+        System.out.println();
     }
 
     /**
@@ -24,5 +28,15 @@ public class StepSecond {
      * compare pre and post increment/decrement programmatically to find difference between them.
      * For comparing create a variable with initial value and use all increments/decrements,
      * line by line with printing result of operation and variable value after.
-     */
+     */ {
+        // unary operators ++/-- increments/decrements a value by 1
+        double a = 10;
+        double b = (--a) / 2 + (b=++a) ;
+        System.out.println("***************Task 2***************");
+        System.out.println("pre and post increment/decrement test");
+        System.out.println("the result of (--a) / 2 + (b=++a) or (--10)/2 + (++9) = " + b);
+        System.out.println("************************************");
+        System.out.println();
+    }
+
 }
---
+++ src/main/java/syntax/StepThird.java
@@ -0,0 +1,111 @@
+package syntax;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class StepThird {
+    public static void main(String[] args) {
+        StepThird stepThird = new StepThird();
+        stepThird.compareInputtedInt();
+        stepThird.compareInput();
+        stepThird.checkEvenOddDouble();
+        stepThird.checkConditionsForFloat();
+
+    }
+
+    /**
+     * Task 1:
+     * create a program, that will scan int value from console and print first message if value less than 10,
+     * second message if larger, than 10 and if it is equal to 0 - third message.
+     */
+    public void compareInputtedInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any digest type int");
+        try {
+            int a = scanner.nextInt();
+            if (a == 0) {
+                System.out.println("a equal to 0");
+            } else if (a < 10) {
+                System.out.println("a less than 10");
+            } else if (a > 10) {
+                System.out.println("a grater than 10");
+            } else
+                System.out.println("not expected value");
+        } catch (Exception e) {
+            System.out.println("Unsupported data type");
+        }
+    }
+
+    public void compareInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any digest type double");
+        String a = scanner.nextLine();
+        try {
+            if (a.contains(".")) {
+                if (Double.valueOf(a) == 0l) {
+                    System.out.println("a equal to 0");
+                } else if (Double.valueOf(a) > 10l) {
+                    System.out.println("a grater than 10");
+                } else if (Double.valueOf(a) < 10l) {
+                    System.out.println("a less than 10");
+                } else {
+                    System.out.println("not expected value");
+                }
+            } else if (Integer.valueOf(a) instanceof Integer) {
+                if (Integer.valueOf(a) == 0) {
+                    System.out.println("a equal to 0");
+                } else if (Integer.valueOf(a) < 10) {
+                    System.out.println("a less than 10");
+                } else if (Integer.valueOf(a) > 10) {
+                    System.out.println("a grater than 10");
+                } else
+                    System.out.println("not expected value");
+            }
+        } catch (Exception e) {
+            System.out.println("Unsupported data type");
+        }
+    }
+
+    /**
+     * Task 2:
+     * create a program, that will scan double value from console and if value is odd - first message
+     * printing to console, if even - second message printing to console.
+     */
+    public void checkEvenOddDouble() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            double dbl = scanner.nextDouble();
+            if (dbl % 2 == 0) {
+                System.out.println("Entered number is even");
+            } else {
+                System.out.println("Entered number is odd");
+            }
+        } catch (Exception e) {
+            System.out.println("Unsupported data type");
+        }
+    }
+
+
+    /**
+     * Task 3:
+     * create a program, that will scan float value from console and if this value is divisible without
+     * remainder by 2 and larger than 10 or if value equals 15 - first message is printing to the console,
+     * if not - second message is printing to console.
+     */
+    public void checkConditionsForFloat() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            float floatVar = scanner.nextFloat();
+            if ((floatVar % 2 == 0 && floatVar > 10) || floatVar == 15) {
+                System.out.println("Entered digit is satisfied required conditions: " +
+                        "divisible without remainder by 2 AND grater than 10 OR equal 15");
+            } else {
+                System.out.println("Entered digit is not satisfied required conditions");
+            }
+        } catch (Exception e) {
+            System.out.println("Unsupported data type");
+        }
+    }
+
+
+}
