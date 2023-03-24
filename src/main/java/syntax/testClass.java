96536dad49b60f3a89ea074c2838b95bb6a1e1ef
lena <cherchenko.olena@airslate.com>
2023-03-24 19:24:21 +0100

    Test branch pushh

--- src/main/java/syntax/testClass.java
+++ src/main/java/syntax/testClass.java
@@ -1,4 +1,29 @@
 package syntax;
 
-public class testClass {git
+import java.util.Stack;
+
+public class testClass {
+    int i = 10;
+    double d = 12;
+
+
+
+    public static void main(String[] args) {
+        Test ts = new Test("TestName+122121");
+        String s = ts.getName().toString();
+        int d = 10;
+        System.out.printf("The test class has name %d, and I printed it in console",10) ;
+    }
+
+    static class Test{
+        private String name;
+
+        Test(String name){
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
