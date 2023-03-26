599526c8f14a55d4aa402a130deddcc29be74948
lena <cherchenko.olena@airslate.com>
2023-03-26 14:26:05 +0200

    4th task was done, looking for better implementation

--- src/main/java/syntax/StepSixth.java
+++ src/main/java/syntax/StepSixth.java
@@ -7,9 +7,10 @@ import java.util.ArrayList;
 
 public class StepSixth {
     public static void main(String[] args) throws IOException {
-        stringFormatPrintAscColumn(); //task 1 test
-        stringFormatPrintEvenWordUppercase();//task 2 test
-        countNumberOfSymbolInString();
+//        stringFormatPrintAscColumn();        //task 1 test
+//        stringFormatPrintEvenWordUppercase();//task 2 test
+//        countNumberOfSymbolInString();       //task 3 test
+        separateStringAndPrint();            //task 4 test
 
     }
 
@@ -118,9 +119,61 @@ public class StepSixth {
      * words by “ ” symbol, before each uppercase letter, like in a sentence.
      * Print the new sentence to the console.
      */
+    public static void separateStringAndPrint() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter string in PascalCase.");
+        String str = reader.readLine();
+        if (str.isEmpty()) {
+            System.out.println("Empty string was entered.");
+        } else if (str.contains(" ")) {
+            System.out.println("Wrong string format, expected PascalCase.");
+        } else if (!str.contains(" ") && !str.isEmpty()) {
+            int counter = 0;
+            char[] charArray = str.toCharArray();
+            for (int i = 0; i < charArray.length; i++) {
+                if (Character.isUpperCase(charArray[i])) {
+                    if (!Character.isUpperCase(charArray[0])) {
+                        counter = -1;
+                        break;
+                    }
+                    counter++;
+                }
+            }
+            switch (counter) {
+                case -1:
+                    System.out.println("Wrong format string.");
+                    break;
+                case 0:
+                    System.out.println("The string does not contains symbols in Uppercase.");
+                    break;
+                case 1:
+                    System.out.println("The string contains only one word starts from Uppercase, cannot be parsed.");
+                    break;
+                default:
+                    ArrayList<String> wordsArray = new ArrayList<>();
+                    int index = 0;
+                    for (int i = 0; i < charArray.length; i++) {
+                        if (Character.isUpperCase(charArray[i])) {
+                            wordsArray.add(str.substring(index, i));
+                            index = i;
+                        }
+                        if (i == (charArray.length - 1)) {
+                            wordsArray.add(str.substring(index));
+                        }
+                    }
+                    for (String s : wordsArray) {
+                        System.out.print(s + " ");
+                    }
 
-    /**
-     * Task 5*:
-     * bubble sorting realization.
-     */
+            }
+
+        }
+
+    }
+
+/**
+ * Task 5*:
+ * bubble sorting realization.
+ */
+
 }
